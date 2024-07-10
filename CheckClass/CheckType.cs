3d036ff14bd31de583f01538a8d2194247a3ffae
sergxlove <sergxlove@mail.ru>
2024-07-10 22:52:43 +0600

    add version methods get string

--- CheckClass/CheckType.cs
+++ CheckClass/CheckType.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Reflection;
+using System.Text;
 using System.Xml.Serialization;
 
 namespace CheckClass
@@ -22,6 +23,12 @@ namespace CheckClass
             GetListEvents();
         }
 
+        public string GetInfoTypeString()
+        {
+            StringBuilder result = new StringBuilder();
+            return result.ToString();
+        }
+
         public void GetInfoType(bool isViewConstructors, bool isViewProperties, bool isViewMethods, bool isViewInterfaces, bool isViewFields, bool isViewEvents)
         {
             GetStatsType();
@@ -37,8 +44,15 @@ namespace CheckClass
         {
             Console.WriteLine($"Name : {_type.Name}");
             Console.WriteLine($"Namespace : {_type.Namespace}");
-            Console.WriteLine($"FillName : {_type.FullName}");
+            Console.WriteLine($"FullName : {_type.FullName}");
             Console.WriteLine();
+        }
+
+        private string GetStatsTypeString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append($"Name : {_type.Name} \nNamespace : {_type.Namespace} \nFullName : {_type.FullName}\n\n");
+            return result.ToString();
         }
 
         private void GetListConstructors()
@@ -59,6 +73,27 @@ namespace CheckClass
             Console.WriteLine();
         }
 
+        private string GetListConstructorsString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("List constructors : \n");
+            var listConstructors = _type.GetConstructors();
+            if (listConstructors.Length != 0)
+            {
+                foreach (var item in listConstructors)
+                {
+                    result.Append($"{item} \n");
+                }
+            }
+            else
+            {
+                result.Append("No found\n");
+            }
+            result.Append('\n');
+            return result.ToString();
+
+        }
+
         private void GetListProperties()
         {
             Console.WriteLine("List properties :");
@@ -75,6 +110,26 @@ namespace CheckClass
                 Console.WriteLine("No found");
             }
             Console.WriteLine();
+        }
+
+        private string GetListPropertiesString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("List properties :");
+            var listPropeerties = _type.GetProperties();
+            if (listPropeerties.Length != 0) 
+            {
+                foreach (var item in listPropeerties)
+                {
+                    result.Append($"{item} \n");
+                }
+            }
+            else
+            {
+                result.Append("No found\n");
+            }
+            result.Append('\n');
+            return result.ToString();
         }
 
         private void GetListMethods()
@@ -110,6 +165,42 @@ namespace CheckClass
             Console.WriteLine();
         }
 
+        private string GetListMethodsString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("List methods :\n");
+            string modificator = "";
+            var listMethods = _type.GetMethods();
+            if (listMethods.Length != 0)
+            {
+                foreach(var item in listMethods)
+                {
+                    if (item.IsPublic)
+                        modificator = "public ";
+                    else if (item.IsPrivate)
+                        modificator = "private ";
+                    else if (item.IsAssembly)
+                        modificator = "internal ";
+                    else if (item.IsFamily)
+                        modificator = "protected ";
+                    else if (item.IsFamilyAndAssembly)
+                        modificator = "private protected ";
+                    else if (item.IsFamilyOrAssembly)
+                        modificator = "protected internal ";
+                    if (item.IsStatic)
+                        modificator = "static";
+                    result.Append($"{modificator} {item} \n");
+                }
+            }
+            else
+            {
+                result.Append("No found \n");
+            }
+            result.Append('\n');
+            return result.ToString();
+
+        }
+
         private void GetListInterfaces()
         {
             Console.WriteLine("List interfaces : ");
@@ -126,6 +217,26 @@ namespace CheckClass
                 Console.WriteLine("No found");
             }
             Console.WriteLine();
+        }
+
+        private string GetListInterfacesString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("List interfaces :\n");
+            var listInterfaces = _type.GetInterfaces();
+            if(listInterfaces.Length != 0)
+            {
+                foreach(var item in listInterfaces)
+                {
+                    result.Append($"{item} \n");
+                }
+            }
+            else
+            {
+                result.Append('\n');
+            }
+            result.Append('\n');
+            return result.ToString();
         }
 
         private void GetListFields()
@@ -146,6 +257,26 @@ namespace CheckClass
             Console.WriteLine();
         }
 
+        private string GetListFieldsString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("List field \n");
+            var listFields = _type.GetFields();
+            if(listFields.Length != 0)
+            {
+                foreach( var item in listFields)
+                {
+                    result.Append($"{item} \n");
+                }
+            }
+            else
+            {
+                result.Append("No found \n");
+            }
+            result.Append('\n');
+            return result.ToString();
+        }
+
         private void GetListEvents()
         {
             Console.WriteLine("List events : ");
@@ -162,6 +293,26 @@ namespace CheckClass
                 Console.WriteLine("No found");
             }
             Console.WriteLine();
+        }
+
+        private string GetListEventsString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("List events \n");
+            var listEvents = _type.GetEvents();
+            if (listEvents.Length != 0)
+            {
+                foreach (var item in listEvents)
+                {
+                    result.Append($"{item} \n");
+                }
+            }
+            else
+            {
+                result.Append("No found \n");
+            }
+            result.Append('\n');
+            return result.ToString();
         }
 
         private readonly Type _type;
